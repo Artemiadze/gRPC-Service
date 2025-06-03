6374b5fbc96c668dbffdb3f3e5f4191e510dc766
Artemiadze <t0mi.v@yandex.ru>
2025-06-03 12:09:34 +0300

    configuring the logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,20 +1,60 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Artemiadze/gRPC-Service/internal/config"
+	"go.uber.org/zap"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
-	// инициализация обьекта config
+	// для запуска go run cmd/sso/main.go --config=./config/local.yaml
 
+	// загрузка конфигурации
 	cfg := config.MustLoad()
 
-	fmt.Println(cfg)
-	// инициализация logger
+	// инициализация логгера
+	logger := setupLogger(cfg.Env)
+	defer logger.Sync() // flush буфера
+
+	logger.Info("starting service",
+		zap.String("env", cfg.Env),
+		zap.String("storage_path", cfg.StoragePath),
+	)
+	//logger.Debug("Debug message")
 
 	// инициализация приложения (app)
 
-	// запуск gRPC-сервер приложения
+	// запустить gRPC-сервер приложения
+}
+
+func setupLogger(env string) *zap.Logger {
+	var logger *zap.Logger
+	var err error
+
+	switch env {
+	case envLocal:
+		cfg := zap.NewDevelopmentConfig()
+		logger, err = cfg.Build()
+	case envDev:
+		cfg := zap.NewDevelopmentConfig()
+		cfg.Encoding = "json"
+		logger, err = cfg.Build()
+	case envProd:
+		cfg := zap.NewProductionConfig()
+		logger, err = cfg.Build()
+	default:
+		logger = zap.NewExample() // fallback logger
+	}
+
+	if err != nil {
+		// В случае ошибки инициализации логгера — паника
+		panic("cannot initialize zap logger: " + err.Error())
+	}
+
+	return logger
 }
