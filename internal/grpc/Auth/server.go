a9d059d133ece9d1eb95de3465c72ae330bba182
Artemiadze <t0mi.v@yandex.ru>
2025-06-16 18:17:06 +0300

    make handlers

--- internal/grpc/Auth/server.go
+++ internal/grpc/Auth/server.go
@@ -5,12 +5,9 @@ import (
 
 	ssov1 "github.com/Artemiadze/gRPC-Service/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
-
-type serverAPI struct {
-	ssov1.UnimplementedAuthServer
-	auth Auth
-}
 
 // Интерфейс, который мы передавали в grpcApp
 type Auth interface {
@@ -25,19 +22,50 @@ type Auth interface {
 		email string,
 		password string,
 	) (userID int64, err error)
+	IsAdmin(
+		ctx context.Context,
+		userID int64,
+		appID int,
+	) (isAdmin bool, err error)
 }
 
-func Register(gRPCServer *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPCServer, &serverAPI{})
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+	auth Auth
+}
+
+const (
+	emptyValue = 0
+)
+
+func Register(gRPCServer *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPCServer, &serverAPI{auth: auth})
 }
 
 func (s *serverAPI) Login(
 	ctx context.Context,
 	req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
-	//panic("implemented me")
+
+	// Валидация входящих данных
+	if req.GetEmail() == "" || req.GetPassword() == "" {
+		return nil, status.Error(
+			codes.InvalidArgument, "Email and password must be provided")
+	}
+
+	if req.GetAppId() <= emptyValue {
+		return nil, status.Error(
+			codes.InvalidArgument, "App ID must be a positive integer")
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		//TODO
+		return nil, status.Error(codes.Internal, "Failed to login: "+err.Error())
+	}
+
 	return &ssov1.LoginResponse{
-		Token: req.GetEmail(),
+		Token: token,
 	}, nil
 }
 
@@ -45,7 +73,19 @@ func (s *serverAPI) Register(
 	ctx context.Context,
 	req *ssov1.RegisterRequest,
 ) (*ssov1.RegisterResponse, error) {
-	panic("implemented me")
+	if req.GetEmail() == "" || req.GetPassword() == "" {
+		return nil, status.Error(
+			codes.InvalidArgument, "Email and password must be provided")
+	}
+
+	uid, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO
+		return nil, status.Error(codes.Internal, "failed to register user")
+	}
+
+	return &ssov1.RegisterResponse{UserId: uid}, nil
+
 }
 
 func (s *serverAPI) IsAdmin(
